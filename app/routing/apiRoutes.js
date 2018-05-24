99ae25dee343cc3f8a1fac16b437be96093784b4
Janell Guzman <janellguzman@gmail.com>
2018-05-24 12:29:41 -0700

    Updating files.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,63 +1,28 @@
-// ===============================================================================
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-// ===============================================================================
 
-var tableData = require("../data/tableData");
-var waitListData = require("../data/waitinglistData");
+// Load Data - we're linking our routes to a series of "data" sources.
+// These data sources hold arrays of information in the friends array.
+
+var friends = require("../data/friends.js");
+
+module.exports = function (app) {
+    // API Get Requests
 
 
-// ===============================================================================
-// ROUTING
-// ===============================================================================
+    // A GET route with the url / api / friends.This will 
+    // be used to display a JSON of all possible friends.
+    app.get("/api/friends", function (req, res) {
+        res.json(friends);
+    });
 
-module.exports = function(app) {
-  // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
+    // API POST Requests
 
-  app.get("/api/tables", function(req, res) {
-    res.json(tableData);
-  });
+    // A POST routes / api / friends.This will be used to 
+    // handle incoming survey results.This route will also
+    //  be used to handle the compatibility logic.
 
-  app.get("/api/waitlist", function(req, res) {
-    res.json(waitListData);
-  });
+    app.post("/api/friends", function (req, res) {
+        // Our "server" will respond to requests and provide users with all the "friends"
+        friends.push(req.body);
+    });
 
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
-  // ---------------------------------------------------------------------------
-
-  app.post("/api/tables", function(req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    // req.body is available since we're using the body-parser middleware
-    if (tableData.length < 5) {
-      tableData.push(req.body);
-      res.json(true);
-    }
-    else {
-      waitListData.push(req.body);
-      res.json(false);
-    }
-  });
-
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
-
-  app.post("/api/clear", function() {
-    // Empty out the arrays of data
-    tableData = [];
-    waitListData = [];
-
-    console.log(tableData);
-  });
-};
+}
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,30 +1,26 @@
-// ===============================================================================
-// DEPENDENCIES
-// We need to include the path package to get the correct file path for our html
-// ===============================================================================
+
+// Dependencies
+// Include the path package to get the correct file path for our html. 
 var path = require("path");
 
+// var friends = require("../data/friends.js");
 
-// ===============================================================================
-// ROUTING
-// ===============================================================================
+// Routing: 
+module.exports = function(app){
+    // HTML Get Requests
 
-module.exports = function(app) {
-  // HTML GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases the user is shown an HTML page of content
-  // ---------------------------------------------------------------------------
+    // Get request for the survey page.
+    app.get("/survey", function(req, res){
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+    });
 
-  app.get("/tables", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/tables.html"));
-  });
+    // Get request for the default/catch-all route that leads back to the home page.
+    app.get("/", function(req, res){
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
 
-  app.get("/reserve", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/reserve.html"));
-  });
-
-  // If no matching route is found default to home
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/home.html"));
-  });
+    // Get request for the default/catch-all route that leads back to the home page.
+    app.get("/home", function(req, res){
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
 };

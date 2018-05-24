b79ecc45df0008a9d9ebfb25e6335f5fad860a26
Janell Guzman <janellguzman@gmail.com>
2018-05-24 16:10:45 -0700

    Friends Updates.

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,80 +4,80 @@ var friends = [
         "name": "Ahmed",
         "photo": "https://ia.media-imdb.com/images/M/MV5BMTcxMTQ4MzY2Nl5BMl5BanBnXkFtZTgwMzAxOTY3MjI@._V1_UY317_CR13,0,214,317_AL_.jpg",
         "scores": [
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
         ]
     },
     {
         "name": "Mr. Coffee",
         "photo": "https://i.kinja-img.com/gawker-media/image/upload/s--5B2hP9As--/c_scale,f_auto,fl_progressive,q_80,w_800/u9nnk06i8co1dxhldgcm.jpg",
         "scores":[
-            3,
-            2,
-            3,
-            4,
-            5,
-            2,
-            2,
-            2,
-            4,
-            3
+            "3",
+            "2",
+            "3",
+            "4",
+            "5",
+            "2",
+            "2",
+            "2",
+            "4",
+            "3"
         ]
     },
     {
         "name": "Ms. Whiskers",
         "photo": "https://c2.staticflickr.com/8/7436/28057380621_1c08034302_o.jpg",
         "scores":[
-            5,
-            4,
-            5,
-            5,
-            4,
-            5,
-            4,
-            5,
-            4,
-            5
+            "5",
+            "4",
+            "5",
+            "5",
+            "4",
+            "5",
+            "4",
+            "5",
+            "4",
+            "5"
         ]
     },
     {
         "name": "Baller",
         "photo": "https://www.thewrap.com/wp-content/uploads/2015/06/hbo-ballers-dwayne-johnson-the-rock-car.jpg",
         "scores":[
-            2,
-            3,
-            1,
-            4,
-            3,
-            2,
-            2,
-            4,
-            2,
-            5
+            "2",
+            "3",
+            "1",
+            "4",
+            "3",
+            "2",
+            "2",
+            "4",
+            "2",
+            "5"
         ]
     },
     {
         "name": "Wonder Woman",
         "photo": "https://upload.wikimedia.org/wikipedia/commons/2/2f/Gal_Gadot_2014_Comic_Con1.jpg",
         "scores":[
-            3,
-            3,
-            5,
-            3,
-            2,
-            3,
-            3,
-            5,
-            3,
-            1
+            "3",
+            "3",
+            "5",
+            "3",
+            "2",
+            "3",
+            "3",
+            "5",
+            "3",
+            "1"
         ]
     },
 ]

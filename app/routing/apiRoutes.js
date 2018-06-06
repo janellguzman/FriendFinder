eb96bb6496d6ad540bd9b951a9a5c84c19cac97e
Janell Guzman <janellguzman@gmail.com>
2018-06-05 17:01:10 -0700

    Finaling edits.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,29 +1,53 @@
+// Pull in required dependencies
+var path = require('path');
 
 // Load Data - we're linking our routes to a series of "data" sources.
 // These data sources hold arrays of information in the friends array.
-
 var friends = require("../data/friends.js");
 
-module.exports = function (app) {
-    // API Get Requests
+// Export API routes
+module.exports = function(app) {
 
-
-    // A GET route with the url / api / friends.This will 
+	// A GET route with the url / api / friends.This will 
     // be used to display a JSON of all possible friends.
-    app.get("/api/friends", function (req, res) {
-        res.json(friends);
-    });
+	app.get('/api/friends', function(req, res) {
+		res.json(friends);
+	});
 
-    // API POST Requests
+	// Add new friend entry
+	app.post('/api/friends', function(req, res) {
+		// Capture the user input object
+		var userInput = req.body;
+		var userResponses = userInput.scores;
 
-    // A POST routes / api / friends.This will be used to 
-    // handle incoming survey results.This route will also
-    //  be used to handle the compatibility logic.
+		// Compute best friend match
+		var matchName = '';
+		var matchImage = '';
+		var totalDifference = 10000;
 
-    app.post("/api/friends", function (req, res) {
-        // Our "server" will respond to requests and provide users with all the "friends"
-        friends.push(req.body);
-        console.log(friends);
-    });
+		// Examine all existing friends in the list
+		for (var i = 0; i < friends.length; i++) {
+			// Compute differenes for each question
+			var diff = 0;
+			for (var j = 0; j < userResponses.length; j++) {
+				diff += Math.abs(friends[i].scores[j] - userResponses[j]);
+			}
+			// If lowest difference, record the friend match
+			if (diff < totalDifference) {
+				console.log('Closest match found = ' + diff);
+				console.log('Friend name = ' + friends[i].name);
+				console.log('Friend image = ' + friends[i].photo);
 
-}
+				totalDifference = diff;
+				matchName = friends[i].name;
+				matchImage = friends[i].photo;
+			}
+		}
+
+		// Add new user
+		friends.push(userInput);
+
+		// Send appropriate response
+		res.json({status: 'OK', matchName: matchName, matchImage: matchImage});
+	});
+};
